e0a026cd497e677781f883656ce2fdff0e0c23cb
ShariqKhan96 <30958488+ShariqKhan96@users.noreply.github.com>
2019-07-13 12:57:31 -0700

    13-07-2019

--- app/src/main/java/android/webinnovatives/com/seefgodriver/App.java
+++ app/src/main/java/android/webinnovatives/com/seefgodriver/App.java
@@ -14,12 +14,32 @@ import java.util.concurrent.atomic.AtomicInteger;
 import io.paperdb.Paper;
 
 public class App extends Application {
+    public static final String NOTIFICATION_CHANNEL_ID = "SeefGo_Ch_01";
+
     @Override
     public void onCreate() {
         super.onCreate();
         Paper.init(this);
+        createNotificationChannel();
     }
 
+    private void createNotificationChannel() {
+        long pattern[] = {1000, 1000};
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "SeefGo",
+                    NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.setDescription("");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.GREEN);
+            notificationChannel.setVibrationPattern(pattern);
+            notificationChannel.enableVibration(true);
+            manager.createNotificationChannel(notificationChannel);
+
+            //manager.createNotificationChannel(notificationChannel);
+        }
+    }
 
 }
 
--- app/src/main/java/android/webinnovatives/com/seefgodriver/services/FBMessagingService.java
+++ app/src/main/java/android/webinnovatives/com/seefgodriver/services/FBMessagingService.java
@@ -1,16 +1,23 @@
 package android.webinnovatives.com.seefgodriver.services;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Color;
+import android.media.RingtoneManager;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.webinnovatives.com.seefgodriver.App;
+import android.webinnovatives.com.seefgodriver.Home;
 import android.webinnovatives.com.seefgodriver.R;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -26,39 +33,63 @@ public class FBMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        showNotification(remoteMessage.getData().get("message"));
+        super.onMessageReceived(remoteMessage);
+        Log.e(FBMessagingService.class.getSimpleName(), "onMessageReceived");
+        Map<String, String> data = remoteMessage.getData();
+
+        Log.e("messageBack", remoteMessage.getData().get("title") + " " + remoteMessage.getData().get("message"));
+        String message = data.get("message");
+        String title = data.get("title");
+        if (message != null || title != null)
+            showNotification(message, title);
+//        else
+//            showNotification(remoteMessage.getNotification().getBody(), remoteMessage.getNotification().getTitle());
     }
 
-    private void showNotification(String message) {
-        String NOTIFICATION_CHANNEL_ID = "SeefGo_CH_01";
-        //Intent i = new Intent(this, RosterActivity.class);
-        // PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
-        long pattern[] = {0, 1000, 500, 1000};
-        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    public void showNotification(String message, String title) {
+        Intent dIntent;
+        PendingIntent dismissIntent = null;
+//        String[] title_action = title.split(":");
+//        Log.e("TitleHash", title_action[0] + title_action[1]);
+        long pattern[] = {1000, 1000};
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "SeefGo",
-                    NotificationManager.IMPORTANCE_HIGH);
+        Log.e("title", title);
 
-            notificationChannel.setDescription("");
-            notificationChannel.enableLights(true);
-            notificationChannel.setLightColor(Color.GREEN);
-            notificationChannel.setVibrationPattern(pattern);
-            notificationChannel.enableVibration(true);
-            manager.createNotificationChannel(notificationChannel);
-        }
+        dIntent = new Intent(this, Home.class);
+        dIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        dismissIntent = PendingIntent.getActivity(this, 999, dIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        switch (title_action[1]) {
+//            case "Home":
+//                dIntent = new Intent(this, Home.class);
+//                dIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                dismissIntent = PendingIntent.getActivity(this, 999, dIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//                break;
+//            case "Roster":
+//                dIntent = new Intent(this, NotificationToRosterReceiver.class);
+////                dIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+////                dIntent.putExtra("launch_main", "true");
+//                dismissIntent = PendingIntent.getBroadcast(this, 999, dIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//
+//        }
 
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+        Notification builder = new NotificationCompat.Builder(this, App.NOTIFICATION_CHANNEL_ID)
                 .setAutoCancel(true)
                 .setContentText(message)
-                .setContentTitle("New Opportunity")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                // .setContentIntent(pendingIntent)
-                .setVibrate(pattern);
-        manager.notify(NotificationID.getID(), builder.build());
-    }
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.truck_logo)
+                .setVibrate(pattern)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentIntent(dismissIntent)
+                .setColor(getResources().getColor(R.color.colorPrimaryDark))
+                .setAutoCancel(true)
+                .build();
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(NotificationID.getID(), builder);
 
+
+    }
 }
 
 class NotificationID {
